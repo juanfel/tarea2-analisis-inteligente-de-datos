676907dfae328c0a1cc8818b33dd0fa42fdac3b5
juanfel <jf3dsaber@gmail.com>
2016-06-21 16:56:02 -0400

    Agregada funcion de fss. Adaptada para que funcione con los datos.

--- pregunta2.py
+++ pregunta2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import sklearn.linear_model as lm
+
 #Procesa el dataframe para el uso posterior
 url = 'http://statweb.stanford.edu/~tibs/ElemStatLearn/datasets/prostate.data'
 df = pd.read_csv(url, sep='\t', header=0)
@@ -18,3 +20,33 @@ X = df_scaled.ix[:,:-1]
 N = X.shape[0]
 X.insert(X.shape[1], 'intercept', np.ones(N))
 y = df_scaled['lpsa']
+
+def fss(x, y, names_x, k = 10000):
+    p = x.shape[1]-1
+    k = min(p, k)
+    names_x = np.array(names_x)
+    remaining = range(0, p)
+    selected = [p]
+    current_score = 0.0
+    best_new_score = 0.0
+    while remaining and len(selected)<=k :
+        score_candidates = []
+        for candidate in remaining:
+            model = lm.LinearRegression(fit_intercept=False)
+            indexes = selected + [candidate]
+            x_train = x[indexes]
+            predictions_train = model.fit(x_train, y).predict(x_train)
+            residuals_train = predictions_train - y
+            mse_candidate = np.mean(np.power(residuals_train, 2))
+            score_candidates.append((mse_candidate, candidate))
+        score_candidates.sort()
+        score_candidates[:] = score_candidates[::-1]
+        best_new_score, best_candidate = score_candidates.pop()
+        remaining.remove(best_candidate)
+        selected.append(best_candidate)
+        print "selected = %s ..."%names_x[best_candidate]
+        print "totalvars=%d, mse = %f"%(len(indexes),best_new_score)
+    return selected
+
+names_regressors = ["Lcavol", "Lweight", "Age", "Lbph", "Svi", "Lcp", "Gleason", "Pgg45"]
+fss(X[istrain],y[istrain],names_regressors)
